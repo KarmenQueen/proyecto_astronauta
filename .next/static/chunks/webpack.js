9d57d88df6fc153e4bb0e605b5481b02e382ea47
pavelLuciano <luciano.guzman01@alumnos.uach.cl>
2022-11-18 06:30:21 -0300

    intento, no pescar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "002da2269f18358a"; }
+/******/ 		__webpack_require__.h = function() { return "221eeb9e26f0559b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
